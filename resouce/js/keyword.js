834b640596ed9ab4f850528842fd18d68949927a
unknown <kpfphamquang@gmail.com>
2019-10-28 12:45:56 +0700

    update

--- resouce/js/keyword.js
+++ resouce/js/keyword.js
@@ -7,7 +7,7 @@ const keywords = [
             'resouce/html-random-789/random-13.html',
             'resouce/html-random-789/random-11-a.html',
             'resouce/html-random-789/random-9.html',
-            'resouce/html-random-789/random-7.html',
+            'resouce/html-random-456/random-6.html',
             'resouce/html-random-789/random-10%20-nhac.html',
             'resouce/html-random-789/random-10%20-nhac.html',
             'resouce/html-random-789/random-10.html'],
@@ -24,14 +24,44 @@ const keywords = [
     },
     {
         content: ['vui', 'sướng', 'yeu doi'],
-        link: 'resouce/html-random-789/random-10.html'
+        link: ['resouce/html-random-789/random-10.html',
+        'resouce/html-random-456/random-4-b.html',
+        'resouce/html-random-456/random-4-a.html',
+        'resouce/html-random-456/random-5-b.html',
+        'resouce/html-random-789/random-10.html',
+        'resouce/html-random-789/random-10%20-nhac.html',
+        'resouce/html-random-789/random-14.html',
+        'resouce/html-random-789/random-13.html',
+        'resouce/html-random-789/random-13.html']
     },
     {
         content: ['that tinh', 'thất tình', 'thattinh'],
-        link: '#'
+        link: ['resouce/html-random-456/random-4.html',
+        'resouce/html-random-456/random-5.html',
+        'resouce/html-random-789/random-7.html',
+        'resouce/html-random-789/random-8.html',
+        'resouce/html-random-456/random-4-b.html',
+        'resouce/html-random-789/random-8.html',
+        'resouce/html-random-456/random-4.html',
+        'resouce/html-random-789/random-8.html',
+        'resouce/html-random-789/random-7.html']
     },
     {
         content: ['that nghiep', 'thất nghiệp', 'thatnghiep'],
-        link: 'resouce/html-random-789/random-11.html'
+        link: ['resouce/html-random-789/random-11.html',
+        'resouce/html-random-789/random-11.html',
+        'resouce/html-random-789/random-11-a.html',
+        'resouce/html-random-789/random-11-b.html',
+        'resouce/html-random-789/random-11-a.html',
+        'resouce/html-random-789/random-11.html',
+        'resouce/html-random-789/random-11-b.html',
+        'resouce/html-random-789/random-11.html',
+        'resouce/html-random-789/random-11-b.html']
+    },
+    {
+        content:['nong gian','nong','nóng','tuc','tức','bực','buc','rất bực','rat buc','buc xuc','bức xúc',
+                'đấm nhau','đấm','dam nhau','xông pha','xong pha','gây gổ','gay go','solo','chửi ','chửi nhau','chui nhau',
+            'quyết chiến ','quyet chien','sống chết'],
+        link: []
     }
 ]
